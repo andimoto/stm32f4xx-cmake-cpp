7ba7d751db99be139220ec83d8c7185401d27d1e
Andreas Zeiler <andimoto@posteo.de>
2019-11-28 13:17:40 +0100

    Update stm32_flash.ld, stm32f407vgt.ld, and main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,35 @@
 #include "stm32f4xx.h"
 
-//extern "C"
 int main()
 {
-  // Switch on blue LED on STM32F407Discovery
-int a = 0;
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	int a = 0;
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-  GPIO_InitTypeDef gpio_out = {
-    .GPIO_Pin = GPIO_Pin_2,
-    .GPIO_Mode = GPIO_Mode_OUT,
-    .GPIO_Speed = GPIO_High_Speed,
-    .GPIO_OType = GPIO_OType_PP,
-    .GPIO_PuPd = GPIO_PuPd_NOPULL
-  };
+	GPIO_InitTypeDef gpio_out = {
+		.GPIO_Pin = GPIO_Pin_2,
+		.GPIO_Mode = GPIO_Mode_OUT,
+		.GPIO_Speed = GPIO_High_Speed,
+		.GPIO_OType = GPIO_OType_PP,
+		.GPIO_PuPd = GPIO_PuPd_NOPULL
+	};
 
-  GPIO_Init(GPIOD, &gpio_out);
-  GPIO_WriteBit(GPIOD, GPIO_Pin_2, Bit_SET);
-
+	GPIO_Init(GPIOD, &gpio_out);
+	GPIO_WriteBit(GPIOD, GPIO_Pin_2, Bit_SET);
 
 
-  for (;;)
-  {
-	  a++;
+
+	for (;;)
+	{
+		a++;
 
 
-	  if(a==100000)
-	  {
-		  a = 0;
+		if(a==100000)
+		{
+			a = 0;
 
-		  GPIO_ToggleBits(GPIOD, GPIO_Pin_2);
-	  }
-  };
+			GPIO_ToggleBits(GPIOD, GPIO_Pin_2);
+		}
+	};
 
-  return 0;
+	return 0;
 }

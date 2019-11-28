638b55af7445d140fa85a9e1b6dfebfc3b06fef1
Andreas Zeiler <andimoto@posteo.de>
2019-11-28 16:37:13 +0100

    added itm_write.c

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 int main()
 {
 	int a = 0;
+
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
 	GPIO_InitTypeDef gpio_out = {
@@ -17,17 +18,17 @@ int main()
 	GPIO_WriteBit(GPIOD, GPIO_Pin_2, Bit_SET);
 
 
-
 	for (;;)
 	{
 		a++;
 
 
-		if(a==100000)
+		if(a==500000)
 		{
 			a = 0;
-
-			GPIO_ToggleBits(GPIOD, GPIO_Pin_2);
+			ITM_SendChar( 65 );   //  Send ASCII code 65 = ’A’
+//			printf("Hallo\n");
+//			GPIO_ToggleBits(GPIOD, GPIO_Pin_2);
 		}
 	};
 

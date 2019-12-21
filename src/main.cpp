e97760c9a7b4c3a8d825dc67c884757e9ef98c24
Andreas Zeiler <andimoto@posteo.de>
2019-12-21 14:05:11 +0100

    changed system clock config for 8MHz external osc

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,30 @@
 #include "stm32f4xx.h"
 #include "core_cm4.h"
 
+
+
 extern std::uint32_t ramFunc(std::uint32_t numA, std::uint32_t numB);
 extern std::uint32_t ramFuncCCM(std::uint32_t numA, std::uint32_t numB);
 
 static hal_uc::timer::timConfig timConf(
 		hal_uc::timer::Instance::TIMER2,
-		83,
+		42-1,
 		hal_uc::timer::CounterMode::UP,
-		1000,
-		hal_uc::timer::ClockDiv::DIV2,
+		999,
+		hal_uc::timer::ClockDiv::DIV1,
 		0,
 		true
 		);
 
-void TIM2_IRQHandler(void)
-{
-	NVIC_SetPendingIRQ(TIM2_IRQn);
-	printf("*");
-}
+uint32_t timerValue = 0;
 
+static hal_uc::timer* refTim;
+static hal_uc::rng* refRng;
 
-int main()
+extern "C"
 {
-	unsigned int a = 0;
+void printRandom(void)
+{
 	std::uint32_t randomNumber = 0;
 	std::uint32_t randomNumberA = 0;
 	std::uint32_t randomNumberB = 0;
@@ -39,35 +40,44 @@ int main()
 	std::uint32_t timC = 0;
 	std::uint32_t timD = 0;
 
+	error = refRng->getNumber(randomNumberA);
+	if(error != true)
+		refRng->getNumber(randomNumberB);
+
+	if(error != true)
+	{
+		timA = refTim->get();
+		randomNumber = ramFunc(randomNumberA, randomNumberB);
+		timB = refTim->get();
+		printf("A %X in %u ticks\n", randomNumber, (timB-timA));
+
+		timC = refTim->get();
+		randomNumber = ramFuncCCM(randomNumberA, randomNumberB);
+		timD = refTim->get();
+		printf("B %X in %u ticks\n", randomNumber, (timD-timC));
+	}
+}
+}
+
+int main()
+{
 	hal_uc::rng rand1;
 	hal_uc::timer tim2(timConf);
+
+	refTim = &tim2;
+	refRng = &rand1;
 
 	rand1.start();
 	tim2.start();
 
+	printf("Clock %u\n", SystemCoreClock);
+
 	for (;;)
 	{
-		a++;
-
-		if(a==2000000)
+		if(timerValue >= 999)
 		{
-			a = 0;
-			error = rand1.getNumber(randomNumberA);
-			if(error != true)
-				rand1.getNumber(randomNumberB);
-
-			if(error != true)
-			{
-				timA = tim2.get();
-				randomNumber = ramFunc(randomNumberA, randomNumberB);
-				timB = tim2.get();
-				printf("A %X in %u ticks\n", randomNumber, (timB-timA));
-
-				timC = tim2.get();
-				randomNumber = ramFuncCCM(randomNumberA, randomNumberB);
-				timD = tim2.get();
-				printf("B %X in %u ticks\n", randomNumber, (timD-timC));
-			}
+			timerValue = 0;
+			printRandom();
 		}
 	};
 
